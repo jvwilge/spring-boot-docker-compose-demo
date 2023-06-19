50431242394a1892403ed0c0c266b4a8d04db863
Jeroen van Wilgenburg <jeroen.van.wilgenburg@rabobank.nl>
2023-06-19 08:03:34 +0200

    generated from https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.1.0&packaging=jar&jvmVersion=17&groupId=io.github.jvwilge&artifactId=compose-demo&name=compose-demo&description=Demo%20project%20for%20Spring%20Boot&packageName=io.github.jvwilge.compose-demo&dependencies=web,data-jdbc,docker-compose

---
+++ src/test/java/io/github/jvwilge/composedemo/ComposeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jvwilge.composedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
