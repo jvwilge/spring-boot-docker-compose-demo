5cb302c7e19f394639f6c10c36a1dc294cbe86cb
Jeroen van Wilgenburg <jeroen.van.wilgenburg@rabobank.nl>
2023-06-20 12:03:51 +0200

    Integration test

--- src/test/java/io/github/jvwilge/composedemo/ComposeDemoApplicationTests.java
+++ src/test/java/io/github/jvwilge/composedemo/ComposeDemoApplicationTests.java
@@ -1,13 +1,28 @@
 package io.github.jvwilge.composedemo;
 
+import io.github.jvwilge.composedemo.domain.Car;
+import io.github.jvwilge.composedemo.repository.CarRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ComposeDemoApplicationTests {
+
+	private final CarRepository repository;
+
+	ComposeDemoApplicationTests(@Autowired CarRepository repository) {
+		this.repository = repository;
+	}
 
 	@Test
 	void contextLoads() {
+		final Iterable<Car> result = repository.findAll(Sort.unsorted());
+
+		System.out.println(result);
 	}
 
 }
